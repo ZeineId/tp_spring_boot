6b7d95a42420f2997ec24d8cefc5fca8febc19c1
zeine <zeineidoumou@gmail.com>
2024-04-09 14:21:35 +0200

    ajout du dépot git

---
+++ Z-pedia/src/main/java/zeine/springboot/ZPediaApplication.java
@@ -0,0 +1,13 @@
+package zeine.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZPediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZPediaApplication.class, args);
+	}
+
+}
---
+++ Z-pedia/src/test/java/zeine/springboot/ZPediaApplicationTests.java
@@ -0,0 +1,13 @@
+package zeine.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZPediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
